b416ffce7ebec41bbbd4673e81225f356fe9e33c
WithsCat <da18.06.03@yandex.ru>
2024-12-18 15:01:23 +0300

    Исправлен ekol tv паресер

--- src/ekol_tv.py
+++ src/ekol_tv.py
@@ -1,6 +1,6 @@
 import asyncio
 import aiohttp
-from typing import Union
+from typing import Tuple, Union
 from datetime import datetime, UTC, timedelta, timezone
 from bs4 import BeautifulSoup
 
@@ -9,68 +9,100 @@ from shared.options import SaveOptions, read_command_line_options
 from shared.output import run_parser_out_to_csv
 from shared.utils import fill_finish_date_by_next_start_date, get_monday_datetime, get_node_text, is_none_or_empty, replace_spaces
 
-class HaberGlobalParser(TvParser):
+class EkolTvParser(TvParser):
     __source_url = "https://www.ekoltv.com.tr/yayin-akisi"
     __channel_name = "Ekol TV"
     __channel_logo_url = None
     __response_time_zone = timezone(timedelta(hours=3))
+    __remove_last = True
 
     async def parse_async(self) -> list[TvProgramData]:
         async with aiohttp.ClientSession() as session:
-            async with session.get(self.__source_url) as resp:
-                html_text = await resp.text()
-                return self.__parse_html(html_text)
+            http_urls = self.get_day_urls()    
 
-    def __parse_html(self, html_input: str):
-        html = BeautifulSoup(html_input, 'html.parser')
-        program_days = html.find("div", {"id": "pills-tabContent"})
+            parsed_days_result = await asyncio.gather(*[
+                self.parse_day_async(session, url) for url in http_urls 
+            ])
+
+            sorted_by_dates = sorted(parsed_days_result, key=lambda x: x[0])
+            
+            result = [ 
+                program 
+                for day in sorted_by_dates 
+                    for program in day[1]
+            ]            
+        
+            fill_finish_date_by_next_start_date(result, self.__remove_last)
+            
+            return result
+
+    async def parse_day_async(self, session, http_url) -> Tuple[datetime, list[TvProgramData]]:
+        date_from_url = http_url.split("=")[-1]
+        current_day = datetime.strptime(date_from_url, "%d.%m.%Y").replace(tzinfo=self.__response_time_zone)
+
+        async with session.get(http_url) as resp: 
+            return self.parse_day_html(await resp.text(), current_day)
+        
+    def parse_day_html(self, html_text:str, current_day:datetime):
+        html = BeautifulSoup(html_text, 'html.parser')
         parsed_programs = []
 
-        current_day = get_monday_datetime(self.__response_time_zone)
-        for current_day_programs in program_days.find_all("div", recursive=False):
-            parsed_programs.extend(
-                self.__parse_day_programs(current_day_programs.div, current_day)
+        programs = html.find("div", {"class":"tl-list"}).find_all("div", recursive=False)
+        last_hour = 0
+
+        for program in programs:
+            program = program.div
+            time = program.find("span",{"class": "time"}).next.split(":")
+            hours = int(time[0])
+            minutes = int(time[1])
+
+            program_name = get_node_text(program.find("a",{"class": "title"}))
+
+            if (last_hour > hours):
+                current_day += timedelta(days=1)
+            last_hour = hours
+
+            datetime_start = current_day.replace(
+                hour=hours,
+                minute=minutes,
+                second=0,
+                tzinfo=self.__response_time_zone
             )
-            current_day += timedelta(days=1)
 
-        fill_finish_date_by_next_start_date(parsed_programs)        
-        return parsed_programs
-
-    def __parse_day_programs(self, current_day_programs, current_day):
-        parsed_programs = []
-        for program_info in current_day_programs.find_all("div", recursive=False):
-            program_info = program_info.a
-            datetime_start = self.__parse_time(
-                program_info.find("h4").text, 
-                current_day
-            )
-            show_name = replace_spaces(program_info.find("h5").text)
-
-            parsed_programs.append(TvProgramData(
+            tv_program = TvProgramData(
                 datetime_start,
                 None,
                 self.__channel_name,
-                show_name,
+                program_name,
                 self.__channel_logo_url,
                 None,
                 False
-            ))
+            )
 
-        return parsed_programs
+            parsed_programs.append(tv_program)
 
-    def __parse_time(self, time, current_day) -> datetime:
-        day = time.split(":") #20:00
-        return current_day.replace(
-            hour=int(day[0]),
-            minute=int(day[1]),
-            second=0,
-            tzinfo=self.__response_time_zone
-        )
+        return (current_day, parsed_programs)
+    
+    
+    def get_day_urls(self) -> list[str]:
+        monday = get_monday_datetime(self.__response_time_zone)
+        day_urls = []
+
+        for day_index in range(0, 7):
+            day = monday + timedelta(days=day_index)
+            day_urls.append(
+                self.__source_url + f"?date={day.day}.{day.month}.{day.year}"
+            )
+
+        return day_urls
+    
+    def parse_time(self, time_str: str) -> datetime:
+        return datetime.fromisoformat(time_str)
 
 
 
 
 if (__name__=="__main__"):
     options = read_command_line_options()
-    parser = HaberGlobalParser(options.parser_options)
+    parser = EkolTvParser(options.parser_options)
     run_parser_out_to_csv(parser, options.save_options)
